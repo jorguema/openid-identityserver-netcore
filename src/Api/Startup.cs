e2b5429ce067353f816fcbd2e91cba0d56d7fe2b
Jorge Guerola Martinez <jorge.guerola@pandasoftware.local>
2019-06-17 14:01:30 +0200

    Initial commit

---
+++ src/Api/Startup.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System.Security.Claims;
+using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore()
+                .AddAuthorization()
+                .AddJsonFormatters();
+
+            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+                .AddIdentityServerAuthentication(options =>
+                {
+                    // base-address of your identityserver
+                    options.Authority = "http://localhost:5000";
+
+                    // name of the API resource
+                    options.ApiName = "api1";
+                    options.ApiSecret = "secretapi";
+                    options.RequireHttpsMetadata = false;
+                    options.RoleClaimType = ClaimTypes.Role;
+                });
+
+            services.AddCors(options =>
+            {
+                // this defines a CORS policy called "default"
+                options.AddPolicy("default", policy =>
+                {
+                    policy.WithOrigins("http://localhost:5003")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseCors("default");
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
